a4e8850f10b1fc8bcac17e207940e26072a7d76b
euri10 <benoit.barthelet@gmail.com>
2024-01-05 18:26:37 +0100

    working debug in docker

--- rootfs/etc/nginx/lua/mylua.lua
+++ rootfs/etc/nginx/lua/mylua.lua
@@ -4,20 +4,28 @@
 local debug_sources_dir = os.getenv('DEBUG_SOURCES_DIR')
 _G.emmy = {}
 _G.emmy.fixPath = function(path)
-    return string.gsub(path, '/etc/nginx/', debug_sources_dir..'/')
+    return string.gsub(path, '/etc/nginx/', debug_sources_dir .. '/')
 end
 
 -- Load debugger
 package.cpath = package.cpath .. ';/usr/local/emmy/?.so'
 local dbg = require('emmy_core')
 dbg.tcpListen('localhost', 9966)
-
--- Wait for IDE connection
+--Wait for IDE connection
 dbg.waitIDE()
-local cookie = nil
-if not cookie then
-    ngx.log(ngx.ERR, err)
-    return
+
+
+-- defines a factorial function
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n - 1)
+    end
 end
+
+print("enter a number:")
+print(fact(10))
+ngx.say("hello world 1")
 
 
