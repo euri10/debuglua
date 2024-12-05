ac7be30df2d802a142c9a2259d9a623a3b07d48c
euri10 <benoit.barthelet@gmail.com>
2024-12-05 09:10:35 +0100

    working

--- rootfs/etc/nginx/lua/mylua.lua
+++ rootfs/etc/nginx/lua/mylua.lua
@@ -1,7 +1,9 @@
--- snip
+--snip
 
--- Fix debugger paths
-local debug_sources_dir = os.getenv('DEBUG_SOURCES_DIR')
+print("Debugger starting")
+--Fix debugger paths
+--local debug_sources_dir = os.getenv('DEBUG_SOURCES_DIR')
+local debug_sources_dir = "./rootfs/etc/nginx"
 _G.emmy = {}
 _G.emmy.fixPath = function(path)
     return string.gsub(path, '/etc/nginx/', debug_sources_dir .. '/')
@@ -10,22 +12,13 @@ end
 -- Load debugger
 package.cpath = package.cpath .. ';/usr/local/emmy/?.so'
 local dbg = require('emmy_core')
-dbg.tcpListen('localhost', 9966)
+ dbg.tcpListen('localhost', 9966)
 --Wait for IDE connection
 dbg.waitIDE()
-
-
--- defines a factorial function
-function fact (n)
-    if n == 0 then
-        return 1
-    else
-        return n * fact(n - 1)
-    end
+print("Debugger started")
+for i = 1, 13 do
+    print(i)
 end
-
-print("enter a number:")
-print(fact(10))
-ngx.say("hello world 1")
+ngx.say("hello world dwdfe1")
 
 
